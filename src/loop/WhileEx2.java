1f536a8259b50c09770bde02a05eb587b2b6ef00
JeongMoJin <wlswjdah123@naver.com>
2023-12-23 18:35:28 +0900

    2023-12-23 반복문

---
+++ src/loop/WhileEx2.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count<=10){
+            System.out.println(2*count);
+            count++;
+        }
+    }
+}
